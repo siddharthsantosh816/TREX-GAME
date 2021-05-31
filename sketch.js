e628c471f1bb0f3cd2061bb3ba06a8d6dbd1c72e
Siddharth Santosh <79411827+siddharthsantosh816@users.noreply.github.com>
2021-05-31 18:07:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,27 +4,23 @@ var gameState;
 var database;
 var newName;
 
-var trex, trexImg,trex_running, trex_collided;
+var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
-
 var cloudsGroup, cloudImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5;
 var collideSound, jumpSound;
 var obsX, cldX;
 var backgroundImg1, backgroundImg2;
-
 var score=0,highScore = 0,highScorer,newName;
 var gameOver, restart;
 var touches =[];
 var games,objects;
 
 function preload(){
-  trexImg  = loadImage('images/trex1.png');
   trex_running =   loadAnimation('images/trex1.png','images/trex3.png','images/trex4.png');
   trex_collided = loadAnimation('images/trex_collided.png');
     
   groundImage = loadImage('images/ground2.png');
-  
   cloudImage = loadImage('images/cloud.png');
   
   obstacle1 = loadImage('images/obstacle1.png');
@@ -48,7 +44,7 @@ function setup() {
   score = 0;
   obsX = 0;
   cldX=0;
-  // create inout and buttons and assign gamesate to PLAY  and capture the name from input if button pressed
+  // create input and buttons and assign gamesate to PLAY  and capture the name from input if button pressed
   var input = createInput("Name");
   input.position(500,300);
  
@@ -62,7 +58,7 @@ function setup() {
     trex.visible = true;
     
 });
-  // read the High score from DB and assign to HScore
+  // read the High score from  adatabase nd assign to highScore
   database=firebase.database();
   var databaseRef=database.ref('High Score');
   databaseRef.on("value",function(data){
@@ -77,17 +73,17 @@ function setup() {
 
 function draw() {
   if(score<10000){
-  background(backgroundImg1);
+    background(backgroundImg1);
   }
   else{
-  background(backgroundImg2);
+    background(backgroundImg2);
   }
   textSize(20);
   fill("red");
   text("Your Score: "+score,trex.x+350,50);
-  text("High Score: "+highScore,trex.x+800,80);
-  text("High Scorer: "+highScorer,trex.x+800,50);
-  //console.log(score);
+  text("High Scorer: "+highScorer,trex.x+750,50);
+  text("High Score: "+highScore,trex.x+750,80);
+
   image(groundImage,-displayWidth/2,displayHeight-160,displayWidth*500,40);
  
   drawSprites();
@@ -96,21 +92,19 @@ function draw() {
   }
   if(gameState=== END) {
     games.ends();
-    //compare score an HScore and update. If score is high, update DB with new name an score 
-    if(score>highScore){
+    
+    //compare score and High Score and update database.
+    if(score > highScore){
       highScore=score;
       highScorer=newName;
       database.ref('/').update({
         "High Scorer":highScorer,
         "High Score":score
-      })
+      });
     }
-    
-
     if(mousePressedOver(restart)) {
          games.restart();
          
       }
     }
   }
-   
